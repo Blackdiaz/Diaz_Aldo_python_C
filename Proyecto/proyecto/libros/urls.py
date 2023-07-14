6d69e8a983262bf91a3048467934349ee62835c7
Aldo Diaz <78707814+Blackdiaz@users.noreply.github.com>
2023-07-13 19:45:41 -0700

    Clase 2023-07-13

--- Proyecto/proyecto/libros/urls.py
+++ Proyecto/proyecto/libros/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     
-    path('Inicio', views.Inicio.as_view(), name = 'indexLibros'),
+    path('index', views.Index.as_view(), name = 'index'),
     #path('insertar',views.Inicio.insertar_libro, name='insertar'),
-    path('formulario',views.Formulario.as_view(), name='formulario')
+    path('formulario',views.Formulario.as_view(), name='formulario'),
+    path('eliminar_libro/<int:libro_id>/',views.EliminarLibro.as_view(), name='eliminar_libro')
 ]
--- Proyecto/proyecto/libros/views.py
+++ Proyecto/proyecto/libros/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse, JsonResponse
 from django.views import View
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from .models import Libros
 from .forms import LibroForms
 # Create your views here.
 def index(request):
     return HttpResponse("Hola mundo")
 
-class Inicio(View):
+class Index(View):
     teamplate_name = 'index.html'
     
     def post(self, request):
@@ -26,7 +26,7 @@ class Formulario(View):
         form = LibroForms(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('inicio')
+        return redirect('index')
         
             
         return render(request, self.teamplate_name, {'form': form})
@@ -34,3 +34,12 @@ class Formulario(View):
     def get(self, request):
         form = LibroForms()
         return render(request, self.teamplate_name, {'form': form})
+    
+    
+class EliminarLibro(View):
+    teamplate_name = 'formulario.html'
+    def post(self, request, libro_id):
+        
+        libro = get_object_or_404(Libros,pk=libro_id)
+        libro.delete()
+        return redirect('index')
