5e84915833e89007fb495d3624c2c46ac72023d4
Aldo Diaz <78707814+Blackdiaz@users.noreply.github.com>
2023-08-25 00:24:05 -0600

    Comentarios add

--- Proyecto_entregable_24_Agosto/django_Sistema_Solicitudes/dashboard/views.py
+++ Proyecto_entregable_24_Agosto/django_Sistema_Solicitudes/dashboard/views.py
@@ -13,40 +13,41 @@ from django.contrib import messages
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-    
+#Llamada para el registro del ticket    
 class Formulario(View):
     teamplate_name = 'formulario.html'
     
     def post(self,request):
         submitted = False
         if request.method == "POST":
-            #resumen = request.POST.get('resumen')
-            ##direccion = request.POST.get('direccion')
-            #archivosAdjuntos = request.POST.get('archivosAdjuntos')
-            #print(request.POST,request.FILES)
+            #se genera una copia del reuqest, para agregar mas datos al reuqest posteriormente
             updated_requestPOST = request.POST.copy()
             updated_requestFILES = request.FILES.copy()
 
+            #Se valida los datos del ticket
             ticketsForms = TicketsForms(request.POST)
             if ticketsForms.is_valid():
                 data = ticketsForms.save()
+                #Una vez guaardado el ticket, recuperamos el id y lo agregamos al request que se copio anteriormente
                 updated_requestPOST.update({'idTicket': data.id})
             else:
+                #Imprimimos los errores de los datos de ticket
                 print(ticketsForms.errors)
+            
+            #Validamos si en el ticket contiene un archivos adjunto para continuar
             if len(request.FILES) != 0:
+                #Agregamos al request de manera independiente el nombre del archivo
                 updated_requestPOST.update({'descricpcionArchivo': request.FILES[u'archivos'].name})
-                print("updated",updated_requestPOST)
+                #Validamos los datos del request para agregar un archivo
                 archivosForms = ArchivosForms(updated_requestPOST,updated_requestFILES)
                 if archivosForms.is_valid():
                     archivosForms.save()
                     
                 else:
                     print(archivosForms.errors)
-            #TicketsObject = get_object_or_404(Tickets, =account_number)
-            #account.current_balance = account.current_balance + amount
-            #account.save()
-
             
+
+        #Devolvemos los campos llenables a la vista HTML    
         ticketform = TicketsForms()
         archivosform = ArchivosForms()
         messages.error(request, 'Se registro correctamente')
@@ -58,7 +59,7 @@ class Formulario(View):
         return render(request, self.teamplate_name, {'ticketform': ticketform,'archivosform': archivosform })
     
 
-
+#Llamada para el dashboard que es para el publico
 class Publico(View):
     teamplate_name = 'dashboardPublico.html'
     
@@ -66,6 +67,7 @@ class Publico(View):
     def get(self, request):
         return render(request, self.teamplate_name)
 
+#LLamada para el dashboard que es para lo servidores publicos
 class Servidor(View):
     teamplate_name = 'dashboardServidor.html'
     
@@ -74,6 +76,7 @@ class Servidor(View):
     def get(self, request):
         return render(request, self.teamplate_name)
 
+#Llamada para la visualizacion de todos los tickets
 class ListaTickets(View):
     teamplate_name = 'listaTickets.html'
 
@@ -91,7 +94,8 @@ class ListaTickets(View):
 
         
         return render(request, self.teamplate_name, {'tickets': tickets, 'archivos':archivos})
-    
+
+#Llamada para que un servidor publio actualice los datos de los tickets    
 class UpdateTicket(View):
     teamplate_name = 'listaTickets.html'
 
@@ -99,15 +103,16 @@ class UpdateTicket(View):
     def post(self,request,ticketId):
         
         if request.method == "POST":
-            
+            #Obtenemos los datos del ticket por el id
             ticket = get_object_or_404(Tickets,pk=ticketId)
-            print(ticket.resumen)
+
+            #Actualizamos los datos del ticket que son editables para el servidor publico
             ticket.nivelPrioridad = request.POST.get('nivelPrioridad')
             ticket.statusTicket = request.POST.get('statusTicket')
             ticket.usuarioEncargado = request.POST.get('usuarioEncargado')
             ticket.comentariosEncargado = request.POST.get('comentariosEncargado')
             ticket.save()
-            print(ticket.resumen)
+            #Devolvemos los tickets actualizados a la vista HTML
             tickets = Tickets.objects.all()
             
                 
@@ -135,15 +140,15 @@ def estadisticas_tickets(request):
                                     conteo_dias=Count('dia')
                                 ).order_by('dia')
     sumaDiasTotales = 0
-    print(promedio_ticket_al_dia)
+
+    #Desglosamos el query set en listas para las graficas
     for ticketFecha in promedio_ticket_al_dia:
         sumaDiasTotales += ticketFecha['conteo_dias']
         
     labels_promedio_ticket_al_dia = [registro['dia'].strftime("%d de %B de %Y").replace("'", '"') for registro in promedio_ticket_al_dia]
     data_promedio_ticket_al_dia = [registro['conteo_dias'] for registro in promedio_ticket_al_dia]
 
-    print(labels_promedio_ticket_al_dia)
-    print(data_promedio_ticket_al_dia)
+
 
     return render(request,'estadisticas.html',{
         'total_tickets':total_tickets,
@@ -154,6 +159,7 @@ def estadisticas_tickets(request):
 
     })   
 
+#LLamada para descargar el archivo adunto al ticket
 def descargar_archivo(request, archivo_id):
     archivo = get_object_or_404(Archivos, id=archivo_id)
     response = HttpResponse(archivo.archivos, content_type='application/octet-stream')
